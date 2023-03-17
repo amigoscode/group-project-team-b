8fb3f7619a8fd6cedfd3667408860b463feb6798
benihoud <benihoud@th-brandenburg.de>
2023-03-17 19:32:25 +0100

    init

---
+++ src/test/java/com/amigoscode/dmarkt/DmarktApplicationTests.java
@@ -0,0 +1,13 @@
+package com.amigoscode.dmarkt;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DmarktApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
