dd4d3d1eed268ce785d1b0bc939ac71dfa670dd8
benihoud <benihoud@th-brandenburg.de>
2023-03-17 19:50:37 +0100

    init project

---
+++ src/main/java/com/amigoscode/dmarkt/DmarktApplication.java
@@ -0,0 +1,13 @@
+package com.amigoscode.dmarkt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DmarktApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DmarktApplication.class, args);
+	}
+
+}
